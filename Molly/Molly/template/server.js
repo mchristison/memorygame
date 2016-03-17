29c295e0d94512b8f71464c3f9564b23bfe13b60
Molly Christison <mchristison@nai-nbs.com>
2016-03-17 12:32:17 -0700

    added insta logo, changed font and background color

--- Molly/Molly/template/server.js
+++ Molly/Molly/template/server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 app.get('/', function(req, res){
-res.sendFile(path.join(__dirname + '/memory.html'));
+res.sendFile(path.join(__dirname + '/memory-main.js'));
 });
 
 app.use(express.static('photos'));
